4dbdd77b1316ca252b613e787916c5c0ca6903aa
Nikola Milun <nikola.milun2508@gmail.com>
2022-10-01 14:28:33 +0200

    Some kind of parallel async loading of db data

--- MVVM/ViewModel/EmployeesViewModel.cs
+++ MVVM/ViewModel/EmployeesViewModel.cs
@@ -36,24 +36,46 @@ namespace AsyncApp.ViewModel
         {
             try
             {
-                List<Employee> list = new List<Employee>();
+                List<Task<Employee>> tasks = new List<Task<Employee>>();
                 using (SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-0KT9RPJ;Initial Catalog=Northwind;Integrated Security=True"))
                 {
                     conn.Open();
 
                     SqlCommand selectCmd = new SqlCommand("SELECT * FROM Employees", conn);
-                    SqlDataReader sdr = selectCmd.ExecuteReader();
+                    SqlDataReader sdr = await selectCmd.ExecuteReaderAsync();
 
                     while (sdr.Read())
                     {
-                        Employee ep = await Task.Run(() =>
+                        int employeeID = int.Parse(sdr[0].ToString());
+                        string lastName = sdr[1].ToString();
+                        string firstName = sdr[2].ToString();
+                        string title = sdr[3].ToString();
+                        string titleOfCourtesy = sdr[4].ToString();
+                        DateTime dateOfBirth = Convert.ToDateTime(sdr[5].ToString());
+                        DateTime hireDate = Convert.ToDateTime(sdr[6].ToString());
+                        string address = sdr[7].ToString();
+                        string city = sdr[8].ToString();
+                        string region = sdr[9].ToString();
+                        string postalCode = sdr[10].ToString();
+                        string country = sdr[11].ToString();
+                        string homePhone = sdr[12].ToString();
+                        string extension = sdr[13].ToString();
+                        string image = sdr[14].ToString();
+                        string text = sdr[15].ToString();
+                        int reportsTo = (sdr[16].ToString() == "") ? 0 : int.Parse(sdr[16].ToString());
+                        string photoPath = sdr[17].ToString();
+                        tasks.Add(Task.Run(() =>
                         {
-                            return new Employee(int.Parse(sdr[0].ToString()), sdr[1].ToString(), sdr[2].ToString(), sdr[3].ToString(), sdr[4].ToString(), Convert.ToDateTime((sdr[5].ToString())), Convert.ToDateTime(sdr[6].ToString()), sdr[7].ToString(), sdr[8].ToString(), sdr[9].ToString(), sdr[10].ToString(), sdr[11].ToString(), sdr[12].ToString(), sdr[13].ToString(), sdr[14].ToString(), sdr[15].ToString(), (sdr[16].ToString() == "") ? 0 : int.Parse(sdr[16].ToString()), sdr[17].ToString());
-                        });
-                        list.Add(ep);
+                            return new Employee(employeeID, lastName, firstName, title, titleOfCourtesy, dateOfBirth, hireDate, address, city, region, postalCode, country, homePhone, extension, image, text, reportsTo, photoPath);
+                        }));
                     }
 
-                    Employees = list;
+                    var results = await Task.WhenAll(tasks);
+
+                    foreach (Employee employee in results)
+                    {
+                        Employees.Add(employee);
+                    }
                 }
             }
             catch (Exception)
